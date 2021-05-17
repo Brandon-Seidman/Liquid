02b90cb222a3508b70cefc3d5c95deadc865b138
Brandon-Seidman <43582959+Brandon-Seidman@users.noreply.github.com>
2021-05-17 17:14:45 -0400

    Updated to mongo atlas

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -5,12 +5,14 @@ const mongoConfig = {
   database: "liquidData",
 };
 
+const uri = "mongodb+srv://liquidappteam:PatrickHill554@cluster0.5uigu.mongodb.net/liquidDatabase?retryWrites=true&w=majority"
+
 let _connection = undefined;
 let _db = undefined;
 
 module.exports = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+    _connection = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -19,3 +21,16 @@ module.exports = async () => {
 
   return _db;
 };
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://liquidappteam:<password>@cluster0.5uigu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+//mongodb+srv://liquidappteam:<password>@cluster0.5uigu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
